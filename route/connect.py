cb3b90a2c8c15b744455b02c1e651e6c2ab58c7f
Ashley <randomrainbw@gmail.com>
2026-01-20 13:07:07 +0200

    Update connect.py
    ei enään vaihda mitä tietokantaa käyttää riippuen mistä activoi tiedoston

--- route/connect.py
+++ route/connect.py
@@ -2,8 +2,11 @@ from os import path
 import sqlite3
 
 # ei huolehdi virheistä, voisi olla fiksumpi
+db_name = "kpedu_tira_db.sqlite" # tietokanta tiedoston nimi
+main_folder = (path.dirname(path.dirname(__file__))) # ottaa aina pääkansion
+tira_db = path.join(main_folder, db_name)
+print(tira_db) # polu tietokantaan
 
-tira_db = path.abspath("../kpedu_tira_db.sqlite") # koko polu tietokantaan
 tira_con = sqlite3.connect(tira_db) # yhteys tietokantaan
 tira_cur = tira_con.cursor()
 
