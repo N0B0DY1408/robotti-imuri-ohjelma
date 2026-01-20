856e5dac83637bbbaa030774791b7d50572a02ed
Ashley <randomrainbw@gmail.com>
2026-01-20 13:37:50 +0200

    Update hash.py

--- route/hash.py
+++ route/hash.py
@@ -6,8 +6,6 @@
 import connect # connect.py
 import bcrypt
 
-# minulle on ilmoitettu että tämä tiedosto on mahdollisesti turha yippii!
-
 def save_password(password):
     # käytetty kun käyttäjä tekee tilin
     bytepassword = password.encode("utf-8")
@@ -16,11 +14,18 @@ def save_password(password):
     return user_salt, hashed_password
     # salt, häsh = tallenna_salasana("salasana")
 
-def use_password(password, username):
+def use_password(password, username): # username on vaan että tietää mikä tili on
     # käytetty kun käyttäjä menee tiliin
     bytepassword = password.encode("utf-8")
-    saved_hash = "placeholder"
-    saved_salt = "placeholder"
-    new_hash = bcrypt.hashpw(bytepassword, saved_salt)
-    return (new_hash == saved_hash) # jos on oikea salasana tulee True
-    # ei vielä valmis
+    saved_salt, saved_hash = connect.tira_cur.execute(f"""
+        SELECT salasana_salt, salasana_hash FROM käyttäjät WHERE käyttäjänimi='{username}'
+    """).fetchone()
+
+    byte_salt = saved_salt.encode("utf-8")
+    new_hash = bcrypt.hashpw(bytepassword, byte_salt)
+    string_new_hash = new_hash.decode("utf-8")
+    # bcrypt hashpw tekee byte häshin, se pitää muuttaa stringiksi jotta voi tallentaa tietokantaan
+    return (saved_hash == string_new_hash) # jos on oikea salasana tulee True
+
+#    testvalue = use_password("pingas", "rordon gamsay")
+#    print(testvalue)
