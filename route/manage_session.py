fbb628a0dce7afac5a14b65cd4bea04aec4d24a4
Ashley <randomrainbw@gmail.com>
2026-01-26 12:38:02 +0200

    Update manage_session.py

--- route/manage_session.py
+++ route/manage_session.py
@@ -1,29 +1,18 @@
-from flask import Flask, redirect, request, session
+from flask import session
 from flask_session import Session
-import connect
 
-app = Flask(__name__)
+def isloggedin():
+    """
+    tämä functio tarkistaa jos käyttäjä on kirjautunut
+    None tarkoittaa että ei ole
+    """
+    if not session.get("email"):
+        return None
+    return session["email"]
 
-app.config["SESSION_TYPE"] = "filesystem"     # Väliaikainen vaihta db jossain vaiheesa
-Session(app)
-
-Session(app)
-
-@app.route("/")
-def index():
-    if not session.get("name"):
-        return "<p>need to login</p>"
-    return f"<p>logged in as {session["name"]}</p>"
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    session["name"] = "testName"
-    return redirect("/")
-
-@app.route("/logout")
-def logout():
-    session["name"] = None
-    return redirect("/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def set_session(user_email=None):
+    """
+    tämä functio asettaa käyttäjän session, sessio käyttää emailii
+    jos ei pane mitään user_mail kohtaan tämä on logout juttu
+    """
+    session["email"] = user_email
