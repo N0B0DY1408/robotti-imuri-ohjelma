6ddeebed6e37e1b72284dce68e560a1ab3d7a9a1
Ashley <randomrainbw@gmail.com>
2026-02-02 13:45:28 +0200

    Update connect.py

--- route/connect.py
+++ route/connect.py
@@ -6,7 +6,7 @@ main_folder = (path.dirname(path.dirname(__file__))) # ottaa aina pääkansion
 tira_db = path.join(main_folder, "kpedu_tira_db.sqlite") # tira db tiedosto
 template_folder = path.join(main_folder, "templates") # templates kansio
 
-tira_con = sqlite3.connect(tira_db) # yhteys tietokantaan
+tira_con = sqlite3.connect(tira_db, check_same_thread=False) # yhteys tietokantaan
 tira_cur = tira_con.cursor()
 
 # import connect
