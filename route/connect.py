99a1e601cb0a9ae3e1abe2d881af348d7014462f
Ashley <randomrainbw@gmail.com>
2026-02-04 14:24:20 +0200

    Update connect.py

--- route/connect.py
+++ route/connect.py
@@ -9,6 +9,14 @@ template_folder = path.join(main_folder, "templates") # templates kansio
 tira_con = sqlite3.connect(tira_db, check_same_thread=False) # yhteys tietokantaan
 tira_cur = tira_con.cursor()
 
+def r_sqlite_of(input):
+    # ridiculous sqlite output formatter
+    # kun sqlite antaa listan joissa on tupleita joissa on yks juttu
+    new_list=[]
+    for item in input:
+        new_list.append(item[0])
+    return new_list
+
 # import connect
 # fulltable = connect.tira_cur.execute("SELECT * FROM käyttäjät")
 # print(fulltable.fetchall())
