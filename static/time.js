9fb8ede65a758b8aff11907a3a97f70e4a854fbe
Ashley <randomrainbw@gmail.com>
2026-02-20 20:30:21 +0200

    Update time.js

--- static/time.js
+++ static/time.js
@@ -3,30 +3,34 @@ timer = document.getElementById("timer")
 // ottaa ajan viime varauksesta, sitten päivittää sitä
 
 function stop_watch(time_since) {
-    var countDownDate = new Date(time_since).getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    var distance = now - countDownDate;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    if (hours == 0) {
-        timer.innerHTML = minutes + "m " + seconds + "s ";
-    } else if (days == 0) {
-        timer.innerHTML = hours + "h "
-        + minutes + "m " + seconds + "s ";
+    if (time_since == "vapaa") {
+        timer.innerHTML = "vapaa"
     } else {
-        timer.innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+        var countDownDate = new Date(time_since).getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        var distance = now - countDownDate;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        if (hours == 0) {
+            timer.innerHTML = minutes + "m " + seconds + "s ";
+        } else if (days == 0) {
+            timer.innerHTML = hours + "h "
+            + minutes + "m " + seconds + "s ";
+        } else {
+            timer.innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+        }
+        });
     }
-    });
 }
