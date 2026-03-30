fe6a1a42b38eb334b87ae021eb7c3b5a13af08f2
TwigRaainb <randomrainbw@gmail.com>
2026-03-30 10:22:28 +0300

    parempi login systeemi

--- static/script.js
+++ static/script.js
@@ -15,7 +15,17 @@ const successView = document.getElementById("successView"); // ei käytetä
 const modalTitle = document.getElementById("modalTitle"); 
 // jotta voi piilottaa 2 juttua vaikka suurin osa piilottamisesta on pythonin puolella
 
-
+function toggleModalMode() {
+    if (emailView.style.display == "none") {
+        emailView.style.display = "block";
+        codeView.style.display = "none";
+        modalTitle.innerText = "Kirjoita sähköpostisi";
+    } else {
+    emailView.style.display = "none";
+    codeView.style.display = "block";
+    modalTitle.innerText = "Syötä vahvistuskoodi";
+    };
+};
 
 // EMAIL SUBMIT
 signupForm.addEventListener("submit", async (e) => {
@@ -23,7 +33,6 @@ signupForm.addEventListener("submit", async (e) => {
     // jos signupformista tulee submit tämä aktivoi
 
     const email = document.getElementById("select-email").value;
-    const number = document.getElementById("roomSelect").value;
     const isChecked = checkbox.checked;
 
     if (!isChecked) {
@@ -38,18 +47,15 @@ signupForm.addEventListener("submit", async (e) => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ email: email, number: number }),
-            // vetää sivulta käyttäjän paneman emailin ja huonenumeron
-            // kohdista joinka idt ovat "roomSelect" ja "select-email"
+            body: JSON.stringify({ email: email }),
+            // vetää sivulta käyttäjän paneman emailin
+            // kohdasta jonka id on "select-email"
         });
         const data = await res.json();
         // odottaa että app.py palauttaa jsonify({"success": True})
 
         if (data.success) {
-            emailView.style.display = "none";
-            codeView.style.display = "block";
-            modalTitle.innerText = "Syötä vahvistuskoodi";
-            // näyttää käyttäjälle kohdan jossa voi panna koodin
+            toggleModalMode()
         } else {
             alert(data.message || "Virhe");
             // näyttää virhe viestin, oletuksena "Virhe" mutta jos teette lisää pankaa oma virhe
@@ -68,8 +74,12 @@ signupForm.addEventListener("submit", async (e) => {
 codeForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     // jos codeformista tulee submit tämä aktivoi
-    const code = codeForm.code.value;
-
+    const numb1 = codeForm.num1.value;
+    const numb2 = codeForm.num2.value;
+    const numb3 = codeForm.num3.value;
+    const numb4 = codeForm.num4.value;
+    const code = numb1 + numb2 + numb3 + numb4
+    // se näyttää typerältä mutta se toimii
     try {
         const res = await fetch("/verify", {
             // lähettää käyttäjän verify sivulle, aktivoi python skriptin sillä
@@ -121,7 +131,7 @@ addRoomForm.addEventListener("submit", async (e) => {
         if (data.success) {
             alert(data.message || "Onnistui!")
         $('#addRoomModal').modal('hide');
-        // piilottaa kirjaudu kohdan vaikka monet muut jutut piilotetaan pythonilla
+        // piilottaa huone kohdan heti kun on valmis
         location.reload();
         // refreshaa sivun
         } else {
@@ -173,6 +183,39 @@ varausForm.addEventListener("submit", async (e) => {
 
 });
 
+const inputs = document.getElementById("inputs");
+
+inputs.addEventListener("input", function (e) {
+    const target = e.target;
+    const val = target.value;
+
+    if (isNaN(val)) {
+        target.value = "";
+        return;
+    }
+
+    if (val != "") {
+        const next = target.nextElementSibling;
+        if (next) {
+            next.focus();
+        }
+    }
+});
+
+inputs.addEventListener("keyup", function (e) {
+    const target = e.target;
+    const key = e.key.toLowerCase();
+
+    if (key == "backspace" || key == "delete") {
+        target.value = "";
+        const prev = target.previousElementSibling;
+        if (prev) {
+            prev.focus();
+        }
+        return;
+    }
+});
+
 new TomSelect("#select-email", {
     create: true,
     no_results:false,
@@ -196,31 +239,6 @@ new TomSelect("#select-email", {
 }
 
 });
-
-new TomSelect("#roomSelect", {
-    create: true,
-    no_results:false,
-    sortField: {
-        field: "text",
-        direction: "asc"
-        
-        
-    },
-    render: {
-    option_create: function(data, escape) {
-        return '<div class="create">lisää huone "<strong>' + 
-            escape(data.input) +
-            '</strong>"</div>';
-    },
-    no_results: function(data, escape) {
-        return '<div class="create">ei tuloksia "<strong>' + 
-            escape(data.input) + 
-            '</strong>"</div>';
-    }
-}
-    
-});
-
 
 new TomSelect("#new_room_num", {
     create: true,
